527fc9a2cedc07b8e0a42e4e59b8093a787f12fc
Atul Thakre <thakre.atul26@gmail.com>
2025-07-10 14:37:22 +0530

    Initial Changes

--- test_enhance_resume.py
+++ test_enhance_resume.py
@@ -20,6 +20,9 @@ def get_zip_path():
 def get_png_path():
     return os.path.abspath(r"C:\Users\HP\Downloads\ChatGPT Image Mar 31, 2025, 12_39_39 PM.png")
 
+def get_jpg_path():
+    return os.path.abspath(r"C:\Users\HP\Downloads\Pancard (1).jpg")
+
 # Reusable fixture for login and setup
 @pytest.fixture
 def driver():
@@ -211,3 +214,43 @@ def test_png_file(driver):
     time.sleep(3)
 
     page_text = driver.page_source.lower()
+
+def test_jpg_file(driver):
+    driver.get("https://demo.aceint.ai/resume")
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "/html/body/div/div[2]/div/main/div/div/main/div[2]/div[1]/div/div/div/div/div"))
+    ).click()
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[contains(.,'Choose job role')]"))
+    ).click()
+    
+    WebDriverWait(driver, 5).until(
+        EC.element_to_be_clickable((By.XPATH,"//div[@role='option']"))
+    )
+
+    for option in driver.find_elements(By.XPATH, "//div[@role='option']"):
+       if "Associate Software Engineer" in option.text:  
+         option.click()
+         break
+
+
+    upload_input = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH,"//input[@type='file']"))
+    )
+
+    upload_input.send_keys(get_jpg_path())
+    time.sleep(5)
+
+    proceed_btn = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH,"/html/body/div[3]/button[1]"))
+    )
+
+    proceed_btn.click()
+
+    time.sleep(3)
+
+    page_text = driver.page_source.lower()
+
+
